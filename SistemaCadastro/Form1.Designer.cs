9531f9a69fea645b17ecf4255c3050f7b84b42cf
Cleyton Gravito <cleytong122@gmail.com>
2024-10-14 15:54:59 -0300

    Atualizações de Layout

--- SistemaCadastro/Form1.Designer.cs
+++ SistemaCadastro/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -56,9 +57,9 @@
             this.label1.Location = new System.Drawing.Point(14, 24);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 26);
+            this.label1.Size = new System.Drawing.Size(171, 26);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Nome";
+            this.label1.Text = "Nome Completo";
             // 
             // label2
             // 
@@ -95,9 +96,10 @@
             // 
             // txtNome
             // 
+            this.txtNome.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtNome.Location = new System.Drawing.Point(206, 24);
-            this.txtNome.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtNome.Margin = new System.Windows.Forms.Padding(2);
             this.txtNome.Name = "txtNome";
             this.txtNome.Size = new System.Drawing.Size(465, 32);
             this.txtNome.TabIndex = 4;
@@ -106,7 +108,7 @@
             // 
             this.txtData.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtData.Location = new System.Drawing.Point(206, 63);
-            this.txtData.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtData.Margin = new System.Windows.Forms.Padding(2);
             this.txtData.Name = "txtData";
             this.txtData.Size = new System.Drawing.Size(465, 32);
             this.txtData.TabIndex = 5;
@@ -116,16 +118,17 @@
             this.comboEC.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboEC.FormattingEnabled = true;
             this.comboEC.Location = new System.Drawing.Point(206, 107);
-            this.comboEC.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboEC.Margin = new System.Windows.Forms.Padding(2);
             this.comboEC.Name = "comboEC";
             this.comboEC.Size = new System.Drawing.Size(338, 34);
             this.comboEC.TabIndex = 6;
             // 
             // txtTelefone
             // 
+            this.txtTelefone.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtTelefone.Location = new System.Drawing.Point(206, 153);
-            this.txtTelefone.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtTelefone.Margin = new System.Windows.Forms.Padding(2);
             this.txtTelefone.Mask = "(00) 00000-0000";
             this.txtTelefone.Name = "txtTelefone";
             this.txtTelefone.Size = new System.Drawing.Size(338, 32);
@@ -135,7 +138,7 @@
             // 
             this.checkCasa.AutoSize = true;
             this.checkCasa.Location = new System.Drawing.Point(206, 207);
-            this.checkCasa.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkCasa.Margin = new System.Windows.Forms.Padding(2);
             this.checkCasa.Name = "checkCasa";
             this.checkCasa.Size = new System.Drawing.Size(120, 17);
             this.checkCasa.TabIndex = 8;
@@ -146,7 +149,7 @@
             // 
             this.checkVeiculo.AutoSize = true;
             this.checkVeiculo.Location = new System.Drawing.Point(206, 240);
-            this.checkVeiculo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkVeiculo.Margin = new System.Windows.Forms.Padding(2);
             this.checkVeiculo.Name = "checkVeiculo";
             this.checkVeiculo.Size = new System.Drawing.Size(95, 17);
             this.checkVeiculo.TabIndex = 9;
@@ -160,19 +163,19 @@
             this.groupBox1.Controls.Add(this.radioF);
             this.groupBox1.Controls.Add(this.radioM);
             this.groupBox1.Location = new System.Drawing.Point(206, 277);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
             this.groupBox1.Size = new System.Drawing.Size(338, 119);
             this.groupBox1.TabIndex = 10;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Sexo";
+            this.groupBox1.Text = "Sexo:";
             // 
             // radioO
             // 
             this.radioO.AutoSize = true;
             this.radioO.Location = new System.Drawing.Point(9, 89);
-            this.radioO.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioO.Margin = new System.Windows.Forms.Padding(2);
             this.radioO.Name = "radioO";
             this.radioO.Size = new System.Drawing.Size(51, 17);
             this.radioO.TabIndex = 2;
@@ -184,7 +187,7 @@
             // 
             this.radioF.AutoSize = true;
             this.radioF.Location = new System.Drawing.Point(9, 62);
-            this.radioF.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioF.Margin = new System.Windows.Forms.Padding(2);
             this.radioF.Name = "radioF";
             this.radioF.Size = new System.Drawing.Size(67, 17);
             this.radioF.TabIndex = 1;
@@ -197,7 +200,7 @@
             this.radioM.AutoSize = true;
             this.radioM.Checked = true;
             this.radioM.Location = new System.Drawing.Point(9, 30);
-            this.radioM.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioM.Margin = new System.Windows.Forms.Padding(2);
             this.radioM.Name = "radioM";
             this.radioM.Size = new System.Drawing.Size(73, 17);
             this.radioM.TabIndex = 0;
@@ -211,7 +214,7 @@
             this.lista.FormattingEnabled = true;
             this.lista.ItemHeight = 26;
             this.lista.Location = new System.Drawing.Point(19, 488);
-            this.lista.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.lista.Margin = new System.Windows.Forms.Padding(2);
             this.lista.Name = "lista";
             this.lista.Size = new System.Drawing.Size(674, 108);
             this.lista.TabIndex = 11;
@@ -219,8 +222,9 @@
             // 
             // btnCadastrar
             // 
+            this.btnCadastrar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnCadastrar.Location = new System.Drawing.Point(19, 410);
-            this.btnCadastrar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCadastrar.Margin = new System.Windows.Forms.Padding(2);
             this.btnCadastrar.Name = "btnCadastrar";
             this.btnCadastrar.Size = new System.Drawing.Size(189, 61);
             this.btnCadastrar.TabIndex = 12;
@@ -230,8 +234,9 @@
             // 
             // btnExcluir
             // 
+            this.btnExcluir.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnExcluir.Location = new System.Drawing.Point(261, 410);
-            this.btnExcluir.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnExcluir.Margin = new System.Windows.Forms.Padding(2);
             this.btnExcluir.Name = "btnExcluir";
             this.btnExcluir.Size = new System.Drawing.Size(189, 61);
             this.btnExcluir.TabIndex = 13;
@@ -241,8 +246,9 @@
             // 
             // btnLimpar
             // 
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnLimpar.Location = new System.Drawing.Point(503, 410);
-            this.btnLimpar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnLimpar.Margin = new System.Windows.Forms.Padding(2);
             this.btnLimpar.Name = "btnLimpar";
             this.btnLimpar.Size = new System.Drawing.Size(189, 61);
             this.btnLimpar.TabIndex = 14;
@@ -270,10 +276,11 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Cadastro";
+            this.Text = "Cadastro de Usuário";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
